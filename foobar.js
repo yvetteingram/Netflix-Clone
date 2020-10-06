a5385ef41b78d4aea7b12ebcdd0656c5f4f49114
Yvette <52262091+yvetteingram@users.noreply.github.com>
2020-10-06 15:10:24 -0400

    Upload Netflix Clone
    Developed with React, firebase, axios, tmdb

---
+++ foobar.js
@@ -0,0 +1,15 @@
+// tmdb API key - ba765efd6d60a0785865a4a5e3ee3f0a;
+// Firebase web address - https://netflix-clone-54d56.web.app
+
+/* <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+*/
